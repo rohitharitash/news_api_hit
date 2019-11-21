44491e3be6ebc3c76c91636592bd8a1c5710e4ac
rohit <rohit.haritash@gmail.com>
2019-11-21 15:47:34 +0530

    1. Testing merge

--- news_api_hit_new.py
+++ news_api_hit_new.py
@@ -11,6 +11,7 @@ def test_fuc():
 
     key1 = "8f44b094b7bb4ceebc6ba53d21cee940" # this is the change in github server
     key2 = "1200edfb432f488cab980978a64c252e"
+    key3="djwjewkje3838e4u3e3jej3j33e983e"
 
     all_result = []
 
