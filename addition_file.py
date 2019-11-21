286f49d7c65de9c33d0495722d8c77471ef01f26
rohit <rohit.haritash@gmail.com>
2019-11-21 15:10:52 +0530

    1. Testing commit feature

--- addition_file.py
+++ addition_file.py
@@ -3,3 +3,21 @@ import numpy as np
 from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
+
+
+print("useless data")
+
+dataset = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+# split into input (X) and output (y) variables
+X = dataset[:,0:8]
+y = dataset[:,8]
+
+
+
+
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
